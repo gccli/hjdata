ecf13b079d9f3828cddc3d4a457e14418d46f3e7
Jing Li <lijing@example.com>
2016-12-03 18:06:46 +0800

    optimize error tolerance

--- search_mine.sh
+++ search_mine.sh
@@ -10,14 +10,14 @@ last_y=1
 function cleanup() {
     find $outdir -name "*.csv" | xargs rm -f
     rm -f .x .y
-    rm -f /tmp/*.txt
+    rm -f $outdir/tmp/*.txt
 }
 
 function load_xy() {
     [ -f .x ] && last_x=$(cat .x)
     [ -f .y ] && last_y=$(cat .y)
 
-    echo last coordinate: "($last_x,$last_y)"
+    logger -s "Last coordinate: ($last_x,$last_y)"
 }
 
 function save_xy() {
@@ -32,38 +32,62 @@ function process_pic() {
     local x=$(echo $name | awk -F_ '{ print $1 }')
     local y=$(echo $name | awk -F_ '{ print $2 }')
 
-    printf "%-16s" "($x,$y)"
+    local xystr=$(printf "%-16s" "($x,$y)")
     if [ ! -f $output ] ;then
         echo "x,y,level,type" > $output
     fi
 
-    tesseract $file /tmp/$name myconfig 2>/dev/null
+    tesseract $file $outdir/tmp/$name myconfig 2>/dev/null
     sleep 0.1
 
-    local match=$(egrep -o '(Lv|va)[. ]*[0-9]+$' /tmp/$name.txt)
+    local match_line=$(egrep '(Lv|va)[. ]*[0-9]+$' $outdir/tmp/$name.txt | sed -n '1p')
+    local match=$(egrep -o '(Lv|va)[. ]*[0-9]+$' $outdir/tmp/$name.txt | sed -n '1p')
     if [ "$match"x == "x" ]; then
-        echo -e "\033[31mnot match\033[0m tesseract $file $/tmp/$name.txt myconfig"
+        logger -s "$xystr not match - tesseract $file stdout myconfig"
     else
-        modified=
+        modified=0
+        modified_info=
+
+        level_start=$(echo $match_line | egrep -o '^[6][02468]')
         level=$(echo $match | egrep -o '[0-9]+')
+        xystr=$(printf "$xystr %-16s %-12s" "match:[$match]" "level:[$level]")
+
+        #echo
+        #echo "match:    [$match_line]"
+        #echo "level:    [$level_start] [$level]"
+        #echo
 
         if [ $level -gt 80 ]; then
-            echo "$match  $level            bad value"
+            logger -s "$xystr - bad value"
         else
+            div_start=0
+            if [ -n "$level_start" ]; then
+                div_start=6
+            fi
+
             div=$(($level/10))
             rem=$(($level%10))
             if [ $rem -eq 3 ]; then
                 rem=8
-                modified="fixed"
+                modified=1
+                modified_info="fix rem from 3 to 8"
             elif [ $rem -eq 5 ]; then
                 rem=6
-                modified="fixed"
+                modified=1
+                modified_info="fix rem from 5 to 6"
             fi
-            if [ $modified"x" != "x" ]; then
+
+            if [ $div_start -eq 6 -a $div -eq 5 ]; then
+                div=6
+                modified=2
+                modified_info="fixed from $level to $level_start"
+            fi
+
+            if [ $modified -ne 0 ]; then
                 level=$(($div*10+$rem))
             fi
 
-            echo "$match  $level            $modified"
+            logger -s "$xystr $modified_info"
             if [ $level -ge $min_lv ]; then
                 echo "$x,$y,$level," >> $output
             fi
@@ -78,13 +102,14 @@ if [ "$1"x == "cleanupx" ]; then
     cleanup
     exit 0
 fi
+mkdir -p $outdir/tmp
 
 load_xy
 for x in $(seq $last_x 599)
 do
     ix=$((x/10))
-    output=result/"$((10*$ix))-$(($ix*10+9))".csv
-    echo "x:$x result file is $output"
+    output=$outdir/"$((10*$ix))-$(($ix*10+9))".csv
+    logger -s "X=$x result output to $output"
     for y in $(seq $last_y 599)
     do
         name="pic/$x/$x"_"$y.bmp"
