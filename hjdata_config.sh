42ea3808b7a85a37dd16d685bdcfbba6351e7656
Jing Li <lijing@example.com>
2016-12-14 19:24:28 +0800

    add match chinese

--- hjdata_config.sh
+++ hjdata_config.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-xstart=599
-ystart=599
+xstart=0
+ystart=267
 
 export xstart
 export ystart
@@ -11,7 +11,9 @@ export min_lv=58
 
 export pattern_eng='(Lv|va)[. ]*[0-9]+$'
 export pattern_chi='[0-9]+级[油铁硅铜宝]'
-export pattern_chi_sim='级[硅宝铁铜桐洞油锔铢圭筐失]{1}'
+export pattern_label='[油铁硅铜宝]'
+export pattern_level='[0-9]+'
+export pattern_lv='Lv[. ]*[0-9]+$'
 
 function setx() {
     sed -i "s/^xstart.*/xstart=$1/" hjdata_config.sh
--- hjdata_scan.sh
+++ hjdata_scan.sh
@@ -2,121 +2,104 @@
 
 source hjdata_config.sh
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <eng|chi|chi_sim>"
-    exit 1
-fi
-
-feedback=feedback.$1
+feedback=feedback.txt
 touch ${feedback}
 
-function analyze_line() {
+function generate_eng() {
     local x=$1
     local y=$2
+    local path=$3
+    local xystr=$(printf "%-16s" "($x,$y)")
 
-    local match_line=$3
+    outb_eng=result/tmp/${x}/${y}.eng
+    outb_chi=result/tmp/${x}/${y}.chi
+    cmd_eng="tesseract $path $outb_eng eng.config"
+    cmd_chi="tesseract -l chi $path $outb_eng"
+
+    mkdir -p tmp/{nomatch,small,bad}/${x}
+    mkdir -p $(dirname ${outb_eng})
+
+    if [ ! -f ${outb_eng}.txt ]; then
+        ${cmd_eng} 2>/dev/null
+    fi
+    if [ ! -f ${outb_chi}.txt ]; then
+        ${cmd_chi} 2>/dev/null
+    fi
+
+    local match_line_eng=$(egrep "${pattern_eng}" ${outb_eng}.txt | sed -n '1p')
+    local match_line_chi=$(egrep "${pattern_chi}" ${outb_chi}.txt | sed -n '1p')
+
+    if [ "${match_line_eng}"x == "x" -o "${match_line_chi}"x == "x" ]; then
+        logger -s "$xystr not match - ${outb_eng} ${outb_chi}"
+        echo "$x,$y,notmatch" >> ${feedback}
+        mv pic/${x}/${y}.bmp tmp/nomatch/${x}/
+        [ $? -ne 0 ] && exit 1
+        return
+    fi
+
+
     local modified=0
     local modified_info=
 
-    local level_start=$(echo "$match_line" | egrep -o '^[6][02468]')
-    local match=$(echo $match_line | egrep -o '(Lv|va)[. ]*[0-9]+$')
-    local level=$(echo $match | egrep -o '[0-9]+')
+    local match=$(echo ${match_line_eng} | egrep -o "${pattern_eng}")      # only for Lv.##
+    local level_start=$(echo "${match_line_eng}" | egrep -o '^[6][02468]') # start number
+    local level=$(echo $match | egrep -o "${pattern_level}")
 
-    mkdir -p tmp/small/${x}
-    mkdir -p tmp/bad/${x}
+    local match_chi=$(echo ${match_line_chi} | egrep -o "${pattern_chi}")
+    local match_lv=$(echo ${match_line_chi} | egrep -o "${pattern_lv}")
+    local label=$(echo ${match_chi} | egrep -o "${pattern_label}")
+    local level_chi=$(echo ${match_chi} | egrep -o "${pattern_level}")
+    local level_lv=$(echo ${match_chi} | egrep -o "${pattern_level}")
+
+    xystr=$(printf "$xystr %-16s %-12s" "match:[$match]" "level:[$level]")
+
+    echo "eng: line:[${match_line_eng}] match:[$match]"
+    echo "chi: line:[${match_line_chi}] label:[$label] level:[$level_chi] lv:[$level_lv]"
 
     if [ $level -gt 80 ]; then
-        logger -s "bad value for $x,$y move"
+        echo "$x,$y,badvalue" >> ${feedback}
+        logger -s "$xystr bad value, move it"
         mv pic/${x}/${y}.bmp tmp/bad/${x}/
         [ $? -ne 0 ] && exit 1
-    else
-        div_start=0
-        if [ -n "$level_start" ]; then
-            div_start=6
-        fi
-
-        div=$(($level/10))
-        rem=$(($level%10))
-        if [ $rem -eq 3 ]; then
-            rem=8
-            modified=1
-            modified_info="fix rem from 3 to 8"
-        elif [ $rem -eq 5 ]; then
-            rem=6
-            modified=1
-            modified_info="fix rem from 5 to 6"
-        fi
-
-        if [ $div_start -eq 6 -a $div -eq 5 ]; then
-            div=6
-            modified=2
-            modified_info="fixed from $level to $level_start"
-        fi
-
-        if [ $modified -ne 0 ]; then
-            level=$(($div*10+$rem))
-        fi
-
-        logger -s "$xystr $modified_info"
-        if [ $level -ge $min_lv ]; then
-            echo "$x,$y,$level," >> ${feedback}
-        fi
-
-        if [ $level -lt 30 ]; then
-            echo "move to small"
-            mv pic/${x}/${y}.bmp tmp/small/${x}/
-            [ $? -ne 0 ] && exit 1
-        fi
-    fi
-}
-
-function generate_report() {
-    local lang=$1
-    local path=$2
-    local x=$3
-    local y=$4
-
-    local xystr=$(printf "%-16s" "($x,$y)")
-
-    outbase=result/tmp/${x}/${y}.${lang}
-    cmd=
-    pattern=
-    if [ $lang == "eng" ]; then
-        cmd="tesseract $path $outbase eng.config"
-        pattern=${pattern_eng}
-    elif [ $lang == "chi_sim" ]; then
-        cmd="tesseract -l chi_sim $path $outbase"
-        pattern=${pattern_chi_sim}
-    elif [ $lang == "chi" ]; then
-        cmd="tesseract -l chi $path $outbase"
-        pattern=${pattern_chi}
+        return
     fi
 
-    mkdir -p tmp/nomatch/${x}
-    mkdir -p $(dirname $outbase)
-    if [ ! -f ${outbase}.txt ]; then
-        $cmd 2>/dev/null
+    div_start=0
+    if [ -n "$level_start" ]; then
+        div_start=6
     fi
 
-    local match_line=$(egrep "$pattern" $outbase.txt | sed -n '1p')
-    if [ "${match_line}"x == "x" ]; then
-        logger -s "$xystr not match - $cmd"
-        echo "$x,$y," >> ${feedback}
+    div=$(($level/10))
+    rem=$(($level%10))
+    if [ $rem -eq 3 ]; then
+        rem=8
+        modified=1
+        modified_info="fix rem from 3 to 8"
+    elif [ $rem -eq 5 ]; then
+        rem=6
+        modified=1
+        modified_info="fix rem from 5 to 6"
+    fi
 
-        mv pic/${x}/${y}.bmp tmp/nomatch/${x}/
+    if [ $div_start -eq 6 -a $div -eq 5 ]; then
+        div=6
+        modified=2
+        modified_info="fixed from $level to $level_start"
+    fi
+
+    if [ $modified -ne 0 ]; then
+        level=$(($div*10+$rem))
+    fi
+
+    logger -s "$xystr $modified_info"
+    if [ $level -lt 30 ]; then
+        echo "remove to small"
+        mv pic/${x}/${y}.bmp tmp/small/${x}/
         [ $? -ne 0 ] && exit 1
-    else
-        xystr=$(printf "$xystr %-16s" "match:[${match_line}]")
-        logger -s "$xystr"
-
-        if [ $lang == "eng" ]; then
-            analyze_line $x $y "${match_line}"
-        fi
     fi
 }
 
 function scan() {
-    local lang=$1
     logger -s "Last coordinate: ($xstart,$ystart) to ($xmax,$ymax)"
 
     for x in $(seq ${xstart} ${xmax})
@@ -125,7 +108,7 @@ function scan() {
         do
             fullpath="pic/$x/$y.bmp"
             [ ! -f $fullpath ] && continue
-            generate_report ${lang} ${fullpath} ${x} ${y}
+            generate_eng ${x} ${y} ${fullpath}
             sety ${y}
         done
 
@@ -133,5 +116,20 @@ function scan() {
     done
 }
 
-scan $1
-sort -u -V feedback.$1
+if [ -n "$1" ]; then
+    if [ "$1" == "reset" ]; then
+        echo "reset x,y"
+        setx 0
+        sety 0
+    else
+        echo "set x=$1"
+        setx $1
+        if [ -n "$2" ]; then
+            setx $2
+        fi
+    fi
+
+    source hjdata_config.sh
+fi
+
+scan
